4f93ccba5a9e16733dba47db52b36d54fd5cdacd
Threetwosevensixseven <robin.guest@gmail.com>
2022-02-27 14:40:23 -0500

    Working version of FakeESP. This currently responds to a very minimal set of AT commands, and acts as a sink. It doesn't make any connections it is asked to make with AT+CIPSTART, and doesn't do anything with characters it is asked to send with AT_CIPSEND. It can respond to baud queries and baud changes though, which means you can happily run .espbaud and it will keep the PC UART in sync at the same baud.

---
+++ src/dotnet/FakeESP/Program.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FakeESP
+{
+    class Program
+    {
+        public static SerialPort listener;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                string port = (ConfigurationManager.AppSettings["ListeningSerialPort"] ?? "").Trim();
+                string cval = (ConfigurationManager.AppSettings["ListeningSerialBaud"] ?? "").Trim();
+                int baud;
+                int.TryParse(cval, out baud);
+
+                listener = new SerialPort(port, baud);
+                var buffer = new byte[8192];
+                int pos = 0;
+                var enc = Encoding.ASCII;
+                bool connIsOpen = false;
+                bool isReceiving = false;
+                ushort toReceive = 0;
+                byte[] rBuffer = new byte[0];
+                ushort rPos = 0;
+                bool startup = true;
+                //.*?AT\+CIPSTART="(?<Protocol>[A-Z]{3})","(?<Host>[^"]*?)",(?<Port>\d{1,5})\r\n
+                var rStart = new Regex(".*?AT\\+CIPSTART=\"(?<Protocol>[A-Z]{3})\",\"(?<Host>[^\"]*?)\",(?<Port>\\d{1,5})\\r\\n");
+                //.*?AT\+CIPSEND=(?<Count>\d{1,5})\r\n
+                var rSend = new Regex(".*?AT\\+CIPSEND=(?<Count>\\d{1,5})\\r\\n");
+                //.*?AT\+UART(?<Flavour>_CUR|)\?\r\n
+                var rBaudQ = new Regex(@".*?AT\+UART(?<Flavour>_CUR|)\?\r\n");
+                //.*?AT\+UART(?<Flavour>_CUR|_DEF|)=(?<Baud>\d{1,9}),\d,\d,\d,\d\r\n
+                var rBaudSet = new Regex(@".*?AT\+UART(?<Flavour>_CUR|_DEF|)=(?<Baud>\d{1,9}),\d,\d,\d,\d\r\n");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("Yellow = debug info; ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Green = RX text; ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("White = TX text");
+                WriteStatus("Listening on " + port + " at " + baud + " baud...");
+                while (true)
+                {
+                    bool success;
+                    do
+                    {
+                        //try
+                        //{
+                        //    var key = Console.Read();
+                        //    if (key == 13) // Enter
+                        //    {
+                        //        if (isReceiving)
+                        //            WriteStatus("Received " + rPos + " bytes so far");
+                        //        else
+                        //            WriteStatus("Not currently receiving");
+                        //    }
+                        //}
+                        //catch { }
+                        var b = listener.ReadByte(out success);
+                        if (success)
+                        {
+                            if (!isReceiving && b == 0)
+                            {
+                            }
+                            else if (startup && b == 213)
+                            { 
+                                // Work around different 3.01.10 joystick behaviour
+                                // When UART is not directed to joystick port
+                            }
+                            else
+                            {
+                                startup = false;
+                                buffer[pos++] = b;
+                                Console.Write(Convert.ToChar(b));
+                            }
+                            if (isReceiving)
+                            {
+                                rBuffer[rPos++] = b;
+                                //WriteStatus("Received " + rPos + " bytes so far");
+                                toReceive--;
+                                if (toReceive == 0)
+                                {
+                                    isReceiving = false;
+                                    string rval = enc.GetString(rBuffer, 0, rBuffer.Length);
+                                    WriteStatus("Received " + rBuffer.Length + " bytes, sending to printer...");
+                                    SendWrite("\r\nRecv " + rBuffer.Length + " bytes\r\n\r\nSEND OK\r\n");
+                                    WriteStatus("Processing AT commands again");
+                                }
+                                pos = 0;
+                            }
+                            string val = enc.GetString(buffer, 0, pos);
+                            if (isReceiving)
+                            {
+                            }
+                            else if (val.EndsWith("AT\r\n"))
+                            {
+                                pos = 0;
+                                WriteStatus("Received AT test command");
+                                SendWrite("\r\nOK\r\n");
+                            }
+                            else if (rBaudQ.IsMatch(val))
+                            {
+                                pos = 0;
+                                var m = rBaudQ.Match(val);
+                                string flavour = m.Groups["Flavour"].Value ?? "";
+                                string flav = "Baud";
+                                if (flavour == "_CUR")
+                                    flav = "CURrent baud";
+                                WriteStatus(flav + " was queried, returning " + baud);
+                                SendWrite("+UART" + flavour + ":" + baud + ",8,1,0,0\r\nOK\r\n");
+                            }
+                            else if (rBaudSet.IsMatch(val))
+                            {
+                                pos = 0;
+                                var m = rBaudSet.Match(val);
+                                string flavour = m.Groups["Flavour"].Value ?? "";
+                                string baudc = m.Groups["Baud"].Value ?? "";
+                                string flav = "baud";
+                                if (flavour == "_CUR")
+                                    flav = "CURrent baud";
+                                else if ((flavour == "_DEF"))
+                                    flav = "DEFault baud";
+                                int baudn;
+                                if (!int.TryParse(m.Groups["Baud"].Value, out baudn))
+                                {
+                                    SendWrite("\r\nERROR\r\n");
+                                }
+                                else
+                                {
+                                    connIsOpen = true;
+                                    WriteStatus("Preparing to change " + port + " " + flav + " to " + baudn);
+                                    SendWrite("\r\nOK\r\n");
+                                    listener.SetBaud(baudn);
+                                    WriteStatus("Changed " + port + " " + flav + " to " + baudn);
+                                }
+                            }
+                            else if (val.EndsWith("ATE0\r\n"))
+                            {
+                                pos = 0;
+                                WriteStatus("Turning off remote echo");
+                                SendWrite("\r\nOK\r\n");
+                            }
+                            else if (val.EndsWith("AT+CIPCLOSE\r\n"))
+                            {
+                                pos = 0;
+                                if (connIsOpen)
+                                {
+                                    connIsOpen = false;
+                                    WriteStatus("Disconnected previously open connection");
+                                    SendWrite("\r\nOK\r\n");
+                                }
+                                else
+                                {
+                                    WriteStatus("Nothing was connected, so couldn't disconnect");
+                                    SendWrite("\r\nERROR\r\n");
+                                }
+                            }
+                            else if (val.EndsWith("AT+CIPMUX=0\r\n"))
+                            {
+                                pos = 0;
+                                WriteStatus("Turning off connection multiplexing");
+                                SendWrite("\r\nOK\r\n");
+                            }
+                            else if (rStart.IsMatch(val))
+                            {
+                                pos = 0;
+                                var m = rStart.Match(val);
+                                string protocol = m.Groups["Protocol"].Value ?? "";
+                                string host = (m.Groups["Host"].Value ?? "").Trim();
+                                ushort portn;
+
+                                if (protocol != "TCP")
+                                {
+                                    SendWrite("\r\nERROR\r\n");
+                                }
+                                else if (string.IsNullOrEmpty(host))
+                                {
+                                    SendWrite("\r\nERROR\r\n");
+                                }
+                                else if (!UInt16.TryParse(m.Groups["Port"].Value, out portn))
+                                {
+                                    SendWrite("\r\nERROR\r\n");
+                                }
+                                else
+                                {
+                                    connIsOpen = true;
+                                    WriteStatus("Connected to " + host + ":" + portn);
+                                    SendWrite("CONNECT\r\n\r\nOK\r\n");
+                                }
+                            }
+                            else if (rSend.IsMatch(val))
+                            {
+                                pos = 0;
+                                var m = rSend.Match(val);
+                                ushort uval;
+                                if (!UInt16.TryParse(m.Groups["Count"].Value, out uval))
+                                {
+                                    SendWrite("\r\nERROR\r\n");
+                                }
+                                else
+                                {
+                                    toReceive = uval;
+                                    isReceiving = true;
+                                    rBuffer = new byte[toReceive];
+                                    rPos = 0;
+                                    WriteStatus("Waiting to receive " + toReceive + " bytes...");
+                                    SendWrite("\r\nOK\r\n>");
+                                }
+                            }
+                        }
+                    }
+                    while (success);
+                    Thread.Sleep(1);
+                }
+            }
+            finally
+            {
+                if (listener == null)
+                    listener.Dispose();
+                listener = null;
+            }
+        }
+
+        static void SendWrite(string Text)
+        {
+            listener.Write(Text);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(Text);
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+
+        static void WriteStatus(string Text)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            var now = DateTime.Now;
+            Console.WriteLine(now.ToShortDateString() + " " + now.ToString("HH:mm:ss.fff") + ": " + (Text ?? ""));
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+    }
+}
---
+++ src/dotnet/FakeESP/SerialPort.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeESP
+{
+    public class SerialPort : IDisposable
+    {
+        private System.IO.Ports.SerialPort port;
+
+        public SerialPort(string portName, int baudRate)
+        {
+            port = new System.IO.Ports.SerialPort();
+            port.PortName = portName;
+            port.BaudRate = baudRate;
+            port.Parity = System.IO.Ports.Parity.None;
+            port.DataBits = 8;
+            port.StopBits = System.IO.Ports.StopBits.One;
+            port.Handshake = System.IO.Ports.Handshake.None;
+            //port.ReadTimeout = 1;
+            port.Open();
+            //port.DataReceived += Port_DataReceived;
+        }
+
+        public byte ReadByte(out bool Success)
+        {
+            try
+            {
+                if (port != null)
+                {
+                    int v = port.ReadByte();
+                    var b = Convert.ToByte(v);
+                    Success = true;
+                    return b;
+                }
+            }
+            catch
+            {
+            }
+            Success = false;
+            return 0xff;
+        }
+
+        public void Write(byte[] buffer, int offset, int count)
+        {
+            if (port != null)
+                port.Write(buffer, offset, count);
+        }
+
+        public void Write(string text)
+        {
+            if (port != null)
+            {
+                var buff = Encoding.ASCII.GetBytes(text);
+                port.Write(buff, 0, buff.Length);
+            }
+        }
+
+        public void SetBaud(int baudRate)
+        {
+            port.BaudRate = baudRate;
+        }
+
+        private void Port_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string x = e.EventType.ToString();
+        }
+
+        #region IDisposable Support
+
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    if (port != null)
+                    {
+                        if (port.IsOpen)
+                            port.Close();
+                        port.Dispose();
+                        port = null;
+                    }
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~SerialPort()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion IDisposable Support
+    }
+}
